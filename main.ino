1fea8f399544eb860c84568f4c188df7f0068293
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-09-19 11:18:56 -0400

    Create  main.ino

---
+++ main.ino
@@ -0,0 +1,16 @@
+// Created by : Bonnie Zhu
+// Created on : Sep 223
+//
+// This is blink program
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
